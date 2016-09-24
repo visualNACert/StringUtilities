b8d322d47ca4b1243af73f376587040c08abd3ee
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2016-09-24 09:22:52 +0200

    Removed unneeded files

--- Example/Pods/Target Support Files/Pods-StringUtilities-macOS/Pods-StringUtilities-macOS-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_StringUtilities_macOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_StringUtilities_macOSVersionString[];
-
--- Example/Pods/Target Support Files/StringUtilities/StringUtilities-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-#import "StringUtilities.h"
-
-FOUNDATION_EXPORT double StringUtilitiesVersionNumber;
-FOUNDATION_EXPORT const unsigned char StringUtilitiesVersionString[];
-
